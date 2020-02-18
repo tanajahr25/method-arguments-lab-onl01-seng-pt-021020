b3b1c03ba25a982457bc35a951069f30693650c4
Tanajah Rosenbaum <tanajahr25@gmail.com>
2020-02-18 01:52:15 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,16 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+
+def introduction_with_language(name, lang)
+  put "HI, my name is #{name} and I am learning to program in #{lang}."
+end
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name, lang)
+   puts "Hi, my name is #{name} and I am learning to program in #{lang}."
+end
